8766c26caa8a555957e54af869e4b0abfa0dd24d
topshadow <2121718893@qq.com>
2016-10-13 17:57:14 +0800

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import {AngularFire, FirebaseObjectObservable}from 'angularfire2';
 
 @Component( {
 selector:'app-root', 
-templateUrl:'./app.component.html', 
+templateUrl:'./app.component.html',
 styleUrls:['./app.component.css']
 })
 export class AppComponent {
-docs:FirebaseObjectObservable < any > ; 
-title = 'app works!'; 
+docs:FirebaseObjectObservable < any > ;
+title = 'app works ! ';
 constructor(af:AngularFire) {
-this.docs = af.database.object('docs'); 
+this.docs = af.database.object('docs');
 }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,6 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-declare var System: any;
-declare var require: any;
-declare var module: any;
+declare var System:any;
+declare var require:any;
+declare var module:any;
